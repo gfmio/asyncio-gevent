7c9cb17a12b35bae1cefd7c65119b363861b77b6
Frédérique Mittelstaedt <git@gfm.io>
2022-01-10 11:33:28 +0000

    lint

--- tests/test_gevent_on_asyncio.py
+++ tests/test_gevent_on_asyncio.py
@@ -1,9 +1,6 @@
 # import gevent
-
 # gevent.config.loop = "asyncio_gevent.gevent_loop.GeventLoop"
-
 # import gevent.monkey  # noqa: E402
-
 # gevent.monkey.patch_all(
 #     socket=False,
 #     dns=True,
@@ -21,26 +18,17 @@
 #     queue=True,
 #     contextvars=True,
 # )
-
 # import asyncio  # noqa: E402
 # import threading  # noqa: E402
 # import time  # noqa: E402
-
 # import asyncio_gevent  # noqa: E402
-
 # # test_asyncio_on_gevent_will_run_in_dummy_thread
-
-
 # def test_asyncio_on_gevent_will_run_in_dummy_thread_with_asyncio_run():
 #     assert threading.current_thread().name == "MainThread"
-
 #     result = asyncio.run(asyncio_on_gevent_will_run_in_dummy_thread_async("MainThread"))
 #     assert result == 42
-
-
 # def test_asyncio_on_gevent_will_run_in_dummy_thread_with_future_to_greenlet():
 #     assert threading.current_thread().name == "MainThread"
-
 #     greenlet = asyncio_gevent.future_to_greenlet(
 #         asyncio_on_gevent_will_run_in_dummy_thread_async("Dummy-2")
 #     )
@@ -48,21 +36,13 @@
 #     greenlet.join()
 #     result = greenlet.get()
 #     assert result == 42
-
-
 # async def asyncio_on_gevent_will_run_in_dummy_thread_async(expected_name):
 #     assert threading.current_thread().name == expected_name
 #     return 42
-
-
 # # test_asyncio_on_gevent_supports_nested_async_calls
-
-
 # def test_asyncio_on_gevent_supports_nested_async_calls_with_asyncio_run():
 #     result = asyncio.run(asyncio_on_gevent_supports_nested_async_calls_1())
 #     assert result == 42
-
-
 # def test_asyncio_on_gevent_supports_nested_async_calls_with_future_to_greenlet():
 #     greenlet = asyncio_gevent.future_to_greenlet(
 #         asyncio_on_gevent_supports_nested_async_calls_1()
@@ -71,32 +51,20 @@
 #     greenlet.join()
 #     result = greenlet.get()
 #     assert result == 42
-
-
 # async def asyncio_on_gevent_supports_nested_async_calls_1():
 #     result = await asyncio_on_gevent_supports_nested_async_calls_2()
 #     assert result == 100
 #     return 42
-
-
 # async def asyncio_on_gevent_supports_nested_async_calls_2():
 #     result = await asyncio_on_gevent_supports_nested_async_calls_3()
 #     assert result == "it works"
 #     return 100
-
-
 # async def asyncio_on_gevent_supports_nested_async_calls_3():
 #     await asyncio.sleep(1)
 #     return "it works"
-
-
 # # test asyncio_on_gevent_supports_awaiting_greenlets
-
-
 # def test_asyncio_on_gevent_supports_awaiting_greenlets_with_asyncio_run():
 #     asyncio.run(asyncio_on_gevent_supports_awaiting_greenlets_1())
-
-
 # def test_asyncio_on_gevent_supports_awaiting_greenlets_with_future_to_greenlet():
 #     greenlet = asyncio_gevent.future_to_greenlet(
 #         asyncio_on_gevent_supports_awaiting_greenlets_1()
@@ -105,19 +73,13 @@
 #     greenlet.join()
 #     result = greenlet.get()
 #     assert result is None
-
-
 # async def asyncio_on_gevent_supports_awaiting_greenlets_1():
 #     current_thread = threading.current_thread()
 #     result = await asyncio_gevent.greenlet_to_future(
 #         gevent.spawn(asyncio_on_gevent_supports_awaiting_greenlets_2, current_thread)
 #     )
 #     assert result == 42
-
-
 # def asyncio_on_gevent_supports_awaiting_greenlets_2(parent_thread):
 #     assert threading.current_thread() != parent_thread
-
 #     time.sleep(1)
-
 #     return 42
